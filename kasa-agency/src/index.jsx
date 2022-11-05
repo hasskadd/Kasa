a16aa98957268a289bcee9c2a76eb73f66b5de55
hkd92 <hassanekadridodo@gmail.com>
2022-11-05 11:34:30 +0100

    [ADD] Router and pages

--- kasa-agency/src/index.jsx
+++ kasa-agency/src/index.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from './pages/Home';
-
+import About from './pages/About/About';
+import Error from './components/Error';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />
+    <Router>
+      <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='*' element={<Error/>}/>
+      </Routes>
+    </Router>
+    
   </React.StrictMode>
 );
 
---
+++ kasa-agency/src/pages/About/About.jsx
@@ -0,0 +1,6 @@
+
+function About(){
+    return <h1>A propos</h1>
+}
+
+export default About
