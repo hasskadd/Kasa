4e0fb591825eae95ee0d46e550912ba86a59cf1e
hkd92 <hassanekadridodo@gmail.com>
2022-11-21 13:07:53 +0100

    edit error page  and responsive

--- kasa-agency/src/components/Error/index.jsx
+++ kasa-agency/src/components/Error/index.jsx
@@ -1,35 +1,15 @@
-import styled from 'styled-components'
 import { StyledErrorLink } from '../../utils/styles/Atom'
-import colors from '../../utils/styles/colors'
+import '../../utils/context/Main.css'
 
-
-const MainContainer = styled.main`
-margin 0 90px;
-max-width: 1240px;
-display: flex;
-flex-direction: column;
-align-items: center;
-`
-const TitleStyle = styled.h1`
-font-size: 180px;
-margin: 0;
-color: ${colors.primaryOrange};
-`
-const Pstyle = styled.p`
-color: ${colors.primaryOrange};
-font-weight: 500;
-font-size: 30px;
-margin-bottom: 80px;
-`
 
 
 function Error(){
     return (
-        <MainContainer>
-            <TitleStyle>404</TitleStyle>
-            <Pstyle>Oups! La page que vous demandez n'existe pas.</Pstyle>
+        <main className='mainContainerError'>
+            <h1 className='titleError'>404</h1>
+            <p className='pStyleError'>Oups! La page que vous demandez n'existe pas.</p>
             <StyledErrorLink to='/'>Retourner sur la page d’accueil</StyledErrorLink>
-        </MainContainer>
+        </main>
     )
 }
 
--- kasa-agency/src/utils/styles/Atom.jsx
+++ kasa-agency/src/utils/styles/Atom.jsx
@@ -15,10 +15,11 @@ export const StyledLink = styled(Link)`
 export const StyledErrorLink = styled(Link)`
 text-decoration: none;
 font-weight: 500;
-font-size: 18px;
+font-size: 22px;
 color: ${colors.primaryOrange};
 border: 2px solid transparent;
 margin-bottom: 80px;
+width:320px;
 &:active{
     border-bottom:1px solid red;
 }
